2e441e6528e82f7094448358d130864603f40bcd
Lou Parslow <lou.parslow@gmail.com>
2015-08-24 19:12:25 -0600

    'all'

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,7 @@
 require 'dev'
 
 
+task :test_msi_install do
+	puts `msiexec /i /quiet HelloCSharpConsole.msi /Lime HelloCSharpConsole.msi.install.log`
+	puts `msiexec /x /quiet HelloCSharpConsole.msi /Lime HelloCSharpConsole.msi.uninstall.log`
+end
